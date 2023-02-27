2159f8192f9af314be0fb0b415eb3934063fe775
nahuel bustamante <nahuelbus2@gmail.com>
2023-02-27 14:01:22 -0300

    Se corrigue el ejercicio , y se le agrega variable

--- README.md
+++ README.md
@@ -1,10 +1,9 @@
 # Proyecto: Black Cat
 
 > Este proyecto es para el curso de desarrollo web de CODERHOUSE. 
-
+> Se Agrega JavaScript al dia de la fecha 21/2/23, la idea del mismo es agregar el costo de envio y mas adelentante toda la tienda con JS
 ----
 > Se trata de una página web orientado a una Tienda de Mangas y Figuras de Resina de anime.
-
 ----
 ## Herramientas Aplicadas en el Curso:
 - HTML
@@ -13,4 +12,5 @@
 - Subir la web a un Host
 - GitHub
 - SEO
+- JavaScript
 ----
--- addcart.js
+++ addcart.js
@@ -1,17 +1,19 @@
-let envios = parseInt(prompt('Elija la siguiente distancia: 1: 1 a 10km, 2: 11km a 22km, 3: 23km a 50km, 4: +50km'))
 
-if (envios===1){
-        alert('Su envio cuesta: $ 200')
+const envios = (km) => {
+        if (km === 1) {
+                return alert('Su envio cuesta: $ 200')
+        }
+        else if (km === 2) {
+                return alert('Su envio cuesta: $ 300')
+        }
+        else if (km === 3) {
+                return alert('Su envio cuesta: $ 500')
+        }
+        else if (km === 4) {
+                return alert('Su envio cuesta: $ 600')
+        }
+        else {
+                return alert('Error: seleccione del 1 al 4')
+        }
 }
-else if(envios===2){
-        alert('Su envio cuesta: $ 300')
-}
-else if(envios===3){
-        alert('Su envio cuesta: $ 500')
-    }
-else if (envios===4){
-        alert('Su envio cuesta: $ 600')
-    }
-else{
-        alert('Error: seleccione del 1 al 4')
-}
+let km = envios (parseInt(prompt('Elija la siguiente distancia: 1: 1 a 10km, 2: 11km a 22km, 3: 23km a 50km, 4: +50km')))
